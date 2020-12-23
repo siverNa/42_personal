8e85a2c4a096e4964db07c57351eaa7e96c7f84a
siverNa <dhrru123@naver.com>
2020-12-23 22:24:01 +0900

    ft_strlcat.c checked norminette, modified libft.h

--- 42_personal/libft/libft.h
+++ 42_personal/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 19:25:38 by sna               #+#    #+#             */
-/*   Updated: 2020/12/23 16:29:23 by sna              ###   ########.fr       */
+/*   Updated: 2020/12/23 22:22:42 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t	strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
