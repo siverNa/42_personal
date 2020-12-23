4f61a535f9f055d30ce64c04c2d7aa3f258ba639
siverNa <dhrru123@naver.com>
2020-12-23 18:24:54 +0900

    ft_strlcpy checked norminette, change directory name Libft->libft, add header

--- 42_personal/libft/libft.h
+++ 42_personal/libft/libft.h
@@ -6,12 +6,15 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 19:25:38 by sna               #+#    #+#             */
-/*   Updated: 2020/12/22 19:50:09 by sna              ###   ########.fr       */
+/*   Updated: 2020/12/23 16:29:23 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
 
 size_t	strlen(const char *s);
 int		ft_isalpha(int c);
