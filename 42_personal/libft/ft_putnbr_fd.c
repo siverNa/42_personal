da7d8378b232c40aa50b23b4bbd00f1beb03ca93
siverNa <dhrru123@naver.com>
2020-12-29 20:37:07 +0900

    ft_strmapi.c, ft_putchar_fd.c, ft_putstr_fd.c, ft_putendl_fd.c, ft_putnbr_fd.c checked norminette, modify libft.h

---
+++ 42_personal/libft/ft_putnbr_fd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/29 19:45:42 by sna               #+#    #+#             */
+/*   Updated: 2020/12/29 20:06:44 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (fd < 0)
+		return ;
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else
+	{
+		if (n >= 10)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+}
+/*
+** Outputs the integer ’n’ to the given file descriptor.
+*/
