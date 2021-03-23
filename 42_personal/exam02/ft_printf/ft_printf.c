cd7f5090fb6ff5a3089e9f27a63a120eb36d2f46
siverNa <dhrru123@naver.com>
2021-03-23 21:54:15 +0900

    ft_printf leaks clear?

--- 42_personal/exam02/ft_printf/ft_printf.c
+++ 42_personal/exam02/ft_printf/ft_printf.c
@@ -108,11 +108,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 int		put_minus(t_form *form, char **n_buff)
 {
 	int				nbr_len;
+	char			*temp;
 
 	nbr_len = 0;
 	if ((form->type == 'd') && (form->nbr_sign == -1))
 	{
-		*n_buff = ft_strjoin("-", *n_buff);
+		temp = ft_strjoin("-", *n_buff);
+		free(*n_buff);
+		*n_buff = temp;
 		nbr_len = 1;
 	}
 	return (nbr_len);
@@ -152,30 +155,11 @@ char	*ft_baseset(char type)
 	return (0);
 }
 
-/* 필요없는 부분
-int		put_minus2(t_form *form,char **n_buff, int n_len)
-{
-	int				nbr_len;
-
-	nbr_len = 0;
-	if (form->nbr_sign == -1)
-	{
-		if (n_len >= form->width)
-		{
-			*n_buff = ft_strjoin("-", *n_buff);
-			nbr_len = 1;
-		}
-		else if (n_len < form->width)
-			*n_buff[0] = '-';
-	}
-	return (nbr_len);
-}
-*/
-
 int		put_str_width(char **buff, t_form *form)
 {
 	int				i;
 	char			*width;
+	char			*temp;
 
 	if (form->width <= (int)ft_strlen(*buff))
 		return ((int)ft_strlen(*buff));
@@ -189,8 +173,10 @@ int		put_str_width(char **buff, t_form *form)
 		i++;
 	}
 	width[i] = '\0';
-	*buff = ft_strjoin(width, *buff);
+	temp = ft_strjoin(width, *buff);
 	free(width);
+	free(*buff);
+	*buff = temp;
 	return (form->width);
 }
 
@@ -239,7 +225,6 @@ int		print_number(unsigned long long nbr, t_form *form)
 	nbr_len = put_str_prec(nbr, form, &nbr_buff);
 	nbr_len += put_minus(form, &nbr_buff);
 	s_len = put_str_width(&nbr_buff, form);
-	//s_len += put_minus2(form,&nbr_buff, nbr_len);
 	ft_putstr(nbr_buff);
 	free(nbr_buff);
 	return (s_len);
