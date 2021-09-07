fad1bde7cee567cd029bd1b036b4f4284df7ff77
siverNa <dhrru123@naver.com>
2021-09-07 23:45:01 +0900

    pipex 실험파일 만들었으나, 컴파일이 안됨. 수정해야함

---
+++ 42_personal/pipex/practice/pipex.h
@@ -0,0 +1,21 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+# define CHILD	0
+# define FILE_1	argv[1]
+# define FILE_2 argv[4]
+# define CMD_1	argv[2]
+# define CMD_2	argv[3]
+
+typedef struct	s_cmd
+{
+	const char	*cmd;
+	char *const	*argv;
+	char *const	*envp;
+}				t_cmd;
+
+#endif
---
+++ 42_personal/pipex/practice/pipex_prac.c
@@ -0,0 +1,74 @@
+#include "pipex.h"
+
+static void	cmd_init(const char *cmd, t_cmd *strt)
+{
+	char	**chunk;
+
+	chunk = ft_split(cmd, ' ');
+	strt->cmd = ft_strjoin("/bin/", chunk[0]);
+	strt->argv = (char *const *)chunk;
+	strt->envp = NULL;
+}
+
+static void	connect_pipe(int pipefd[2], int io)
+{
+	dup2(pipefd[io], io);
+	close(pipefd[0]);
+	close(pipefd[1]);
+}
+
+int			redirect_in(const char *file)
+{
+	int fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(file);
+		return (-1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+int			redirect_out(const char *file)
+{
+	int fd;
+	fd = open(file, O_RDWR | O_CREAT, 0644);
+	if (fd < 0)
+	{
+		perror(file);
+		return (-1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
+int main(int argc, char const **argv)
+{
+	int		pipefd[2];
+	pid_t	pid;
+	t_cmd	cmd;
+
+	pipe(pipefd);
+	pid = fork();
+	if (pid > 0)
+	{
+		redirect_out(FILE_2);
+		connect_pipe(pipefd, STDIN_FILENO);
+		cmd_init(CMD_2, &cmd);
+		if (execve(cmd.cmd, cmd.argv, cmd.envp) == -1)
+			perror(cmd.cmd);
+	}
+	else if (pid == CHILD)
+	{
+		redirect_in(FILE_1);
+		connect_pipe(pipefd, STDOUT_FILENO);
+		cmd_init(CMD_1, &cmd);
+		if (execve(cmd.cmd, cmd.argv, cmd.envp) == -1)
+			perror(cmd.cmd);
+	}
+	return (0);
+}
