c9a0ec2f0c7629558dd3c57b84ef6cdedae59296
siverNa <dhrru123@naver.com>
2021-09-17 21:17:52 +0900

    pipex 기초 파일 생성

---
+++ 42_personal/pipex/pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 21:07:35 by sna               #+#    #+#             */
+/*   Updated: 2021/09/17 21:16:17 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include "libft.h"
+
+typedef struct	s_cmd
+{
+	int		pipefd[2];
+}				t_cmd;
+
+
+#endif
