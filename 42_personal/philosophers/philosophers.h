15e69ad27b935cdcbc35f2c8bedfbd46c11c5c8c
siverNa <dhrru123@naver.com>
2021-11-03 20:42:03 +0900

    make philosophers.h basic

---
+++ 42_personal/philosophers/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 20:38:02 by sna               #+#    #+#             */
+/*   Updated: 2021/11/03 20:41:38 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
