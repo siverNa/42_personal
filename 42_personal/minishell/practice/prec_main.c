f601fd0afeb90cee81101cbc771bc5893f0c0ed5
siverNa <dhrru123@naver.com>
2022-01-27 22:35:15 +0900

    임시 Makefile 작성, 작동안함 / ctrl+c 시그널 동작하는 코드 작성

--- 42_personal/minishell/practice/prec_main.c
+++ 42_personal/minishell/practice/prec_main.c
@@ -1,20 +1,36 @@
 #include "minishell_prec.h"
 
+void sig_handler(int signum)
+{
+	if (signum != SIGINT)
+		return;
+	printf("\b\bctrl + c\n");
+	rl_on_new_line();
+	rl_replace_line("", 1);
+	rl_redisplay();
+}
+
 int main(void)
 {
+	t_node	node;
 	char	*str;
-	char	**splited;
 	int		i;
 
+	signal(SIGINT, sig_handler);
 	while(1)
 	{
 		str = readline("practice : ");
-		splited = ft_split(str, ' ');
+		node.cmd_line = ft_split(str, ' ');
 		i = 0;
-		while (splited[i])
-			printf("splited[%d] : %s\n", i++, splited[i]);
+		while (node.cmd_line[i])
+			printf("splited[%d] : %s\n", i++, node.cmd_line[i]);
 		add_history(str);
 		free(str);
+		i = 0;
+		while(node.cmd_line[i])
+			free(node.cmd_line[i++]);
+		free(node.cmd_line);
+		str = NULL;
 	}
 	return (0);
 }
