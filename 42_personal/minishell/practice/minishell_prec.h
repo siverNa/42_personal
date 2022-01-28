0d9170250b0ef0c9e8b67e4ed563d51cffd5f8d2
siverNa <dhrru123@naver.com>
2022-01-28 19:01:00 +0900

    구조체에 str 집어넣기 성공, 메모리 누수 체크 완료

--- 42_personal/minishell/practice/minishell_prec.h
+++ 42_personal/minishell/practice/minishell_prec.h
@@ -8,6 +8,8 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <signal.h>
+# define TRUE		1
+# define FALSE		0
 
 typedef struct		s_node
 {
@@ -15,5 +17,22 @@ typedef struct		s_node
 	struct s_node	*prev;
 	struct s_node	*next;
 }					t_node;
+
+typedef struct		s_deq
+{
+	int				size;
+	t_node			*head;
+	t_node			*tail;
+}					t_deq;
+
+/*
+** deque_func.c
+*/
+void	free_cmdline(char **cmdline);
+void	free_deq(t_deq *cmd);
+void	init_cmd(t_deq *deq);
+int		dq_is_empty(t_deq *d);
+void	lst_addend(t_deq *deq, char *str);
+void	insert_str(t_deq *deq, char *str);
 
 #endif
--- 42_personal/minishell/practice/prec_main.c
+++ 42_personal/minishell/practice/prec_main.c
@@ -12,25 +12,50 @@ void sig_handler(int signum)
 
 int main(void)
 {
-	t_node	node;
+	t_deq	cmd;
 	char	*str;
 	int		i;
 
 	signal(SIGINT, sig_handler);
+	init_cmd(&cmd);
 	while(1)
 	{
 		str = readline("practice : ");
-		node.cmd_line = ft_split(str, ' ');
+		//node.cmd_line = ft_split(str, ' ');
+		if (*str == '0')
+		{
+			free(str);
+			str = NULL;
+			break ;
+		}
+		insert_str(&cmd, str);
 		i = 0;
-		while (node.cmd_line[i])
-			printf("splited[%d] : %s\n", i++, node.cmd_line[i]);
+		while (cmd.tail->cmd_line[i])
+			printf("splited[%d] : %s\n", i++, cmd.tail->cmd_line[i]);
 		add_history(str);
 		free(str);
-		i = 0;
-		while(node.cmd_line[i])
-			free(node.cmd_line[i++]);
-		free(node.cmd_line);
 		str = NULL;
 	}
+	i = 0;
+	t_node	*temp;
+	temp = cmd.head;
+	while (i < cmd.size)
+	{
+		int j;
+		j = 0;
+		while (temp->cmd_line[j])
+			printf("stored str : %s\n", temp->cmd_line[j++]);
+		temp = temp->next;
+		i++;
+	}
+	free_deq(&cmd);
 	return (0);
 }
+
+/*
+ comfile command
+ gcc prec_main.c deque_func.c -lreadline 
+-L/home/linuxbrew/.linuxbrew/Cellar/readline/8.1.2/lib 
+-I/home/linuxbrew/.linuxbrew/Cellar/readline/8.1.2/include -L../libft -lft 
+-g3 -fsanitize=address
+*/
