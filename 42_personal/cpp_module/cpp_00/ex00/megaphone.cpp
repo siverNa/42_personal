589525ab9c4b40f7ff40174bcd86750a18305ef3
siverNa <dhrru123@naver.com>
2022-03-23 01:02:16 +0900

    Feat: 추가 cpp_00 ex00 코드
    - ex00 풀이 코드 추가

---
+++ 42_personal/cpp_module/cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 23:04:11 by sna               #+#    #+#             */
+/*   Updated: 2022/03/23 00:04:15 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
