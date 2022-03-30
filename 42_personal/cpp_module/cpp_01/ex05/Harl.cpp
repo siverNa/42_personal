42bc806297b5cf8f39d84e060e9a3d9884992d25
siverNa <dhrru123@naver.com>
2022-03-30 15:10:35 +0900

    Fix: 수정 ex05 Harl.cpp 코드
    - level 출력문 형식 수정

--- 42_personal/cpp_module/cpp_01/ex05/Harl.cpp
+++ 42_personal/cpp_module/cpp_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 00:18:10 by sna               #+#    #+#             */
-/*   Updated: 2022/03/29 23:23:27 by sna              ###   ########.fr       */
+/*   Updated: 2022/03/30 15:10:04 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	Harl::init(void)
 
 void	Harl::debug()
 {
-	std::cout << "@ DEBUG" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon "
 	<< "for my 7XL-double-cheese-triple-pickle-specialketchup burger."
 	<< std::endl << "I really do!"
@@ -53,7 +53,7 @@ void	Harl::debug()
 
 void	Harl::info()
 {
-	std::cout << "@ INFO" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money."
 	<< std::endl << "You didn’t put enough bacon in my burger!"
 	<< std::endl << "If you did, I wouldn’t be asking for more!"
@@ -62,7 +62,7 @@ void	Harl::info()
 
 void	Harl::warning()
 {
-	std::cout << "@ WARNING" << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free."
 	<< std::endl << "I’ve been coming for years whereas you started working here since last month."
 	<< std::endl << std::endl;;
@@ -70,6 +70,6 @@ void	Harl::warning()
 
 void	Harl::error()
 {
-	std::cout << "@ ERROR" << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }
