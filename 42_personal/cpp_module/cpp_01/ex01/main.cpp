daed656a09f24025775ad6a3d8fb2c8643f59393
siverNa <dhrru123@naver.com>
2022-03-30 14:09:25 +0900

    Fix: 수정 ex01 main.cpp 코드
    - for 문 안에서 객체를 호출할 때, 좀 더 명시적으로 보이도록 [i]로 수정

--- 42_personal/cpp_module/cpp_01/ex01/main.cpp
+++ 42_personal/cpp_module/cpp_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 01:52:02 by sna               #+#    #+#             */
-/*   Updated: 2022/03/27 01:55:34 by sna              ###   ########.fr       */
+/*   Updated: 2022/03/30 14:05:06 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(void)
 	zombie_horde = zombieHorde(N, "forHorde");
 	
 	for (int i = 0; i < N; i++)
-		zombie_horde->announce();
+		zombie_horde[i].announce();
 	
 	delete[] zombie_horde;
 
