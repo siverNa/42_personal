a90de951d58c5496437a4de09b3eb31ab0be82fd
siverNa <dhrru123@naver.com>
2022-04-08 01:05:40 +0900

    Feat: 추가 Shru... .hpp, .cpp 코드
    - 문제 조건에 맞게 클래스 구현

---
+++ 42_personal/cpp_module/cpp_05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 20:50:06 by sna               #+#    #+#             */
+/*   Updated: 2022/04/08 00:58:27 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() : Form("none", "ShrubberyCreationForm", 145, 137)
+{
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target) : Form(target, "ShrubberyCreationForm", 145, 137)
+{
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& obj) : Form(obj)
+{
+
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+
+}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& obj)
+{
+	this->Form::operator=(obj);
+	return (*this);
+}
+
+void ShrubberyCreationForm::execute(Bureaucrat const & executor) const
+{
+	if (getSigned() == false)
+		throw IsNotSignedException();
+	if (getExecGrade() > executor.getGrade())
+		throw ExecGradeTooLowException();
+	
+	std::string fileName = getTarget().append("_shrubbery");
+	std::ofstream ofs(fileName);
+	if (ofs.is_open() == false)
+	{
+		std::cout << "Error: fail open <" << fileName << ">" << std::endl;
+		return ;
+	}
+	ofs <<	"-----------,------------------\n"
+			"--------,,,,,,,,,,,,-,,,------\n"
+			"---,,,,,,,,,,,..,,,,,,,,,,,---\n"
+			",,,,,,,,.....,........,,,,,,,,\n"
+			",,,,,,.,...................,,,\n"
+			",,,,,.,......................,\n"
+			",,..............~:~~::~.......\n"
+			",..........   -~!;;;:.:~......\n"
+			".........    ~:;;~!:;!    ....\n"
+			".......      -;;~:!!;:;     ..\n"
+			" .           ~;;!;;!;~;.      \n"
+			"             ~:!!!!=;*;,      \n"
+			"             ~~=;!$=!!::      \n"
+			".            .;*:-! ;*:,      \n"
+			".            ,-   :           \n"
+			",                 !           \n"
+			".                             \n"
+			";;:~~~~~~~-~~~~~:::~~-~~~~:~::\n"
+			"*;;;::::::;:::::~~::::::::::::\n"
+			"!;;;;:;;;;::::;::::::::;:::;::\n";
+	ofs.close();
+}
