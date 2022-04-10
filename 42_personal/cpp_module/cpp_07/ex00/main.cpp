2ecd4f27e7d11359d7e6e11f975ff1885a7ed138
siverNa <dhrru123@naver.com>
2022-04-10 17:23:14 +0900

    Feat: 추가 cpp07 ex00 디렉토리
    - ex00 문제풀이 추가

---
+++ 42_personal/cpp_module/cpp_07/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 20:08:02 by sna               #+#    #+#             */
+/*   Updated: 2022/04/10 17:16:20 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return 0;
+}
---
+++ 42_personal/cpp_module/cpp_07/ex00/whatever.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 15:49:37 by sna               #+#    #+#             */
+/*   Updated: 2022/04/10 17:19:54 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <string>
+
+/*
+** 함수 템플릿 만들기
+** 함수를 만들어 낼때, 함수의 기능은 명확하지만, 자료형을 모호하게 두는 것.
+** 자료형이 들어가는 자리를 T가 대신하는데, int, double 등 다 받을 수있음!
+** templete 은 헤더파일 내에서 정의해줘야함.
+*/
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+#endif
