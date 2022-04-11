63497f13be49662a3da4c3987392d41924343b06
siverNa <dhrru123@naver.com>
2022-04-11 15:37:38 +0900

    Fix: 수정 cpp07 ex01 코드
    - 매개변수로 T const& 를 받도록 수정

--- 42_personal/cpp_module/cpp_07/ex01/iter.hpp
+++ 42_personal/cpp_module/cpp_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 17:40:16 by sna               #+#    #+#             */
-/*   Updated: 2022/04/10 18:17:37 by sna              ###   ########.fr       */
+/*   Updated: 2022/04/11 15:32:53 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 # include <string>
 
 template <typename T>
-void iter(T* arr, std::size_t size, void (*f)(T))
+void iter(T* arr, std::size_t size, void (*f)(T const &))
 {
 	for (std::size_t i = 0; i < size; i++)
 		f(arr[i]);
 }
 
 template <typename T>
-void print(T target)
+void print(T const &target)
 {
 	std::cout << target << std::endl;
 }
