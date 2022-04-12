15b366c7b03e77a894c5c049a7bcac54055403fb
siverNa <dhrru123@naver.com>
2022-04-12 20:50:08 +0900

    Fix: 수정 main, Makefile 코드
    - main 문을 구현한 과제를 테스트할 수 있도록 수정
    - Makefile 수정

--- 42_personal/cpp_module/cpp_08/ex00/main.cpp
+++ 42_personal/cpp_module/cpp_08/ex00/main.cpp
@@ -6,13 +6,57 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:12:58 by sna               #+#    #+#             */
-/*   Updated: 2022/04/12 16:13:00 by sna              ###   ########.fr       */
+/*   Updated: 2022/04/12 20:48:41 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <deque>
 
 int main(void)
 {
+	std::vector<int> v;
+	std::list<int> l;
+	std::deque<int> d;
 
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+		l.push_back(i);
+		d.push_back(i);
+	}
+
+	try
+	{
+		std::vector<int>::iterator iter = easyfind(v, 3);
+		std::cout << "found value " << *iter << " in vector" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::list<int>::iterator iter = easyfind(l, 9);
+		std::cout << "found value " << *iter << " in list" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::deque<int>::iterator iter = easyfind(d, 2);
+		std::cout << "found value " << *iter << " in deque" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return (0);
 }
