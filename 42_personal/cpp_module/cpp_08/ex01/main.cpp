2ab84a5190fc8634de762cb1decc2c1d7098fd4b
siverNa <dhrru123@naver.com>
2022-04-13 15:36:57 +0900

    Fix: 수정 main.cpp 코드
    - main문에 테스트 케이스 추가

--- 42_personal/cpp_module/cpp_08/ex01/main.cpp
+++ 42_personal/cpp_module/cpp_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:12:58 by sna               #+#    #+#             */
-/*   Updated: 2022/04/13 15:01:51 by sna              ###   ########.fr       */
+/*   Updated: 2022/04/13 15:30:35 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int main(void)
 {
+	std::cout << "====================== test1 =====================" << std::endl;
 	Span sp = Span(5);
 
 	sp.addNumber(6);
@@ -25,6 +26,7 @@ int main(void)
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 
+	std::cout << "====================== test2 =====================" << std::endl;
 	Span sp2(9);
 	std::vector<int> temp;
 	for (int i = 0; i < 9; i++)
@@ -34,12 +36,85 @@ int main(void)
 	std::cout << sp2.shortestSpan() << std::endl;
 	std::cout << sp2.longestSpan() << std::endl;
 
+	try
+	{
+		Span sp2_2(9);
+
+		sp2_2.addNumber(12);
+		sp2_2.addNumber(15);
+		std::vector<int> temp2;
+		for (int i = 0; i < 9; i++)
+			temp2.push_back(i + 1);
+
+		sp2_2.addNumber(temp2.begin(), temp2.end());
+		std::cout << sp2_2.shortestSpan() << std::endl;
+		std::cout << sp2_2.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+
+	std::cout << "====================== test3 =====================" << std::endl;
 	Span sp3 = Span(10000);
 
 	for (int i = -5000; i <= 4999; i++)
 		sp3.addNumber(i);
 	std::cout << sp3.shortestSpan() << std::endl;
 	std::cout << sp3.longestSpan() << std::endl;
+
+	std::cout << "====================== test4 =====================" << std::endl;
+	Span sp4 = Span(5);
+	sp4.addNumber(-1);
+	sp4.addNumber(-3);
+	sp4.addNumber(-12);
+	sp4.addNumber(-6);
+	sp4.addNumber(-8);
+	std::cout << sp4.shortestSpan() << std::endl;
+	std::cout << sp4.longestSpan() << std::endl;
+
+	std::cout << "====================== test5 =====================" << std::endl;
+	try
+	{
+		Span sp5 = Span(0);
+		sp5.addNumber(1);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Span sp6 = Span(1);
+		sp6.addNumber(1);
+		sp6.addNumber(2);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	
-	return 0;
+	std::cout << "====================== test6 =====================" << std::endl;
+	try
+	{
+		Span sp7 = Span(1);
+		sp7.addNumber(2);
+		std::cout << sp7.shortestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Span sp8 = Span(0);
+		std::cout << sp8.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	return (0);
 }
