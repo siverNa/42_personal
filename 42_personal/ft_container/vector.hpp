431a5240bb87f084d96dd1a055d4a44ba4f12ac0
siverNa <dhrru123@naver.com>
2022-04-26 22:58:13 +0900

    Feat: 추가 vector.hpp 파일
    - vector.hpp 기초 토대 작성

---
+++ 42_personal/ft_container/vector.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/26 22:52:39 by sna               #+#    #+#             */
+/*   Updated: 2022/04/26 22:56:44 by sna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <iostream>
+# include <math.h>
+
+namespace ft {
+
+	template <typename T, class A = std::allocator<T> >
+	class vector
+	{
+		public: 
+		
+	}
+}
+
+#endif
