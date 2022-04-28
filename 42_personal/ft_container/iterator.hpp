5782184b9dc5544124ca1580e1be1ca79c9366bb
siverNa <dhrru123@naver.com>
2022-04-28 23:54:24 +0900

    Fix: 수정 iterator.hpp 코드
    - iterator_tag 및 iterator_trait 구조체 추가

--- 42_personal/ft_container/iterator.hpp
+++ 42_personal/ft_container/iterator.hpp
@@ -6,13 +6,41 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 23:17:15 by sna               #+#    #+#             */
-/*   Updated: 2022/04/28 23:17:45 by sna              ###   ########.fr       */
+/*   Updated: 2022/04/28 23:53:44 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
 
+# include <cstddef>
+# include <iostream>
 
+namespace ft {
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+	template <class Category, class T, class Distance = ptrdiff_t,
+			  class Pointer = T*, class Reference = T&>
+	struct iterator {
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+		typedef Category	iterator_category;
+	};
+
+	template <class Iterator>
+	struct iterator_traits {
+		typedef typename Iterator::iterator_category	iterator_category;
+		typedef typename Iterator::valuie_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+	};
+}
 
 #endif
