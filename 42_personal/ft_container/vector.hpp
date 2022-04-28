3db8f6c9be8d49f4195d418fe99edcc10eadec15
siverNa <dhrru123@naver.com>
2022-04-28 22:30:34 +0900

    Fix: 수정 vector.hpp 코드
    - templete 및 할당자에 맞춰서 typedef 추가

--- 42_personal/ft_container/vector.hpp
+++ 42_personal/ft_container/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 22:52:39 by sna               #+#    #+#             */
-/*   Updated: 2022/04/26 22:56:44 by sna              ###   ########.fr       */
+/*   Updated: 2022/04/28 22:29:02 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@
 
 namespace ft {
 
-	template <typename T, class A = std::allocator<T> >
+	template <typename T, class Alloc = std::allocator<T> >
 	class vector
 	{
 		public: 
-		
-	}
+			typedef T 											value_type;
+			typedef Alloc 										allocator_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+
+			
+	};
 }
 
 #endif
