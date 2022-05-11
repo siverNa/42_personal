39537e3b10141cadfec48f1c18998d3bde8e4350
siverNa <dhrru123@naver.com>
2022-05-11 22:46:47 +0900

    fix: 수정 vector.hpp 코드
    - typedef 변수들 추가
    - private 변수 추가

--- 42_personal/ft_container/vector.hpp
+++ 42_personal/ft_container/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 22:52:39 by sna               #+#    #+#             */
-/*   Updated: 2022/04/28 22:29:02 by sna              ###   ########.fr       */
+/*   Updated: 2022/05/11 22:45:41 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,92 @@
 # define VECTOR_HPP
 
 # include <iostream>
-# include <math.h>
+# include <memory>
+# include <cstddef>
+
+# include "util.hpp"
+# include "iterator.hpp"
+# include "random_access_iterator.hpp"
 
 namespace ft {
 
+	/**
+	 * @brief Vectors are sequence containers representing arrays that can change in size.
+	 * 
+	 * @tparam T : Type of the elements.
+	 * @tparam Alloc : Type of the allocator object
+	 * used to define the storage allocation model.
+	 */
 	template <typename T, class Alloc = std::allocator<T> >
 	class vector
 	{
 		public: 
+			/**
+			 * @brief The first template parameter (T)
+			 */
 			typedef T 											value_type;
+			/**
+			 * @brief The first template parameter (T)
+			 */
+			typedef T const										const_value_type;
+			/**
+			 * @brief The second template parameter (Alloc),
+			 * defaults to: allocator<value_type>
+			 */
 			typedef Alloc 										allocator_type;
+			/**
+			 * @brief allocator_type::reference
+			 * 	for the default allocator: value_type&
+			 */
 			typedef typename allocator_type::reference			reference;
+			/**
+			 * @brief allocator_type::const_reference
+			 * 	for the default allocator: const value_type&
+			 */
 			typedef typename allocator_type::const_reference	const_reference;
+			/**
+			 * @brief allocator_type::pointer
+			 * 	for the default allocator: value_type*
+			 */
 			typedef typename allocator_type::pointer			pointer;
+			/**
+			 * @brief allocator_type::const_pointer
+			 * 	for the default allocator: const value_type*
+			 */
 			typedef typename allocator_type::const_pointer		const_pointer;
 
-			
+			/**
+			 * @brief a random access iterator to value_type
+			 */
+			typedef typename ft::random_access_iterator<value_type>			iterator;
+			/**
+			 * @brief a random access iterator to const value_type
+			 */
+			typedef typename ft::random_access_iterator<const_value_type>	const_iterator;
+			/**
+			 * @brief reverse_iterator<iterator>
+			 */
+			typedef typename ft::reverse_iterator<iterator>					reverse_iterator;
+			/**
+			 * @brief reverse_iterator<const_iterator>
+			 */
+			typedef typename ft::reverse_iterator<const_iterator>			const_reverse_iterator;
+			/**
+			 * @brief 	a signed integral type, 
+			 * identical to: iterator_traits<iterator>::difference_type
+			 */
+			typedef typename ft::iterator_traits<iterator>::difference_type	difference_type;
+			/**
+			 * @brief an unsigned integral type that can represent
+			 * any non-negative value of difference_type
+			 */
+			typedef typename allocator_type::size_type						size_type;
+		
+		private:
+			allocator_type	_alloc;
+			pointer			_start;
+			pointer			_end;
+			pointer			_end_capacity;
 	};
 }
 
