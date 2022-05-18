566c30edfd57f7529e65693bd9804cc5b8accea7
siverNa <dhrru123@naver.com>
2022-05-18 22:10:03 +0900

    :

--- 42_personal/ft_container/vector.hpp
+++ 42_personal/ft_container/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 22:52:39 by sna               #+#    #+#             */
-/*   Updated: 2022/05/16 23:37:56 by sna              ###   ########.fr       */
+/*   Updated: 2022/05/18 22:09:33 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -216,6 +216,234 @@ namespace ft {
 				this->insert(this->_start, x.begin(), x.end());
 				return (*this);
 			};
+
+			/**
+			 * @brief Return iterator to beginning
+			 * Returns an iterator pointing to the first element in the vector.
+			 */
+			iterator begin() {return (_start); };
+
+			/**
+			 * @brief Return iterator to beginning
+			 * Returns an iterator pointing to the first element in the vector.
+			 */
+			const_iterator begin() const {return (_start); };
+
+			/**
+			 * @brief Return iterator to end
+			 * Returns an iterator referring to the past-the-end element
+			 * in the vector container.
+			 * If the container is empty, this function returns the same as vector::begin.
+			 */
+			iterator end()
+			{
+				if (this->empty())
+					return (this->begin());
+				return (_end);
+			};
+
+			/**
+			 * @brief Return iterator to end
+			 * Returns an iterator referring to the past-the-end element
+			 * in the vector container.
+			 * If the container is empty, this function returns the same as vector::begin.
+			 */
+			const_iterator end() const
+			{
+				if (this->empty())
+					return (this->begin());
+				return (_end);
+			};
+
+			/**
+			 * @brief Return reverse iterator to reverse beginning
+			 * Returns a reverse iterator pointing to the last element in the vector
+			 * (i.e., its reverse beginning).
+			 */
+			reverse_iterator rbegin() {return (reverse_iterator(this->end())); };
+
+			/**
+			 * @brief Return reverse iterator to reverse beginning
+			 * Returns a reverse iterator pointing to the last element in the vector
+			 * (i.e., its reverse beginning).
+			 */
+			const_reverse_iterator rbegin() const {return (reverse_iterator(this->end())); };
+
+			/**
+			 * @brief Return reverse iterator to reverse end
+			 * Returns a reverse iterator pointing to the theoretical element
+			 * preceding the first element in the vector (which is considered its reverse end).
+			 */
+			reverse_iterator rend() {return (reverse_iterator(this->begin())); };
+
+			/**
+			 * @brief Return reverse iterator to reverse end
+			 * Returns a reverse iterator pointing to the theoretical element
+			 * preceding the first element in the vector (which is considered its reverse end).
+			 */
+			const_reverse_iterator rend() const {return (reverse_iterator(this->begin())); };
+
+			/**
+			 * @brief Return size
+			 * Returns the number of elements in the vector.
+			 */
+			size_type size() const {return (this->_end - this->_start); };
+
+			/**
+			 * @brief Return maximum size
+			 * Returns the maximum number of elements that the vector can hold.
+			 */
+			size_type max_size() const {return (this->_alloc.max_size()); };
+
+			/**
+			 * @brief Change size
+			 * Resizes the container so that it contains n elements.
+			 * 
+			 * @param n
+			 * New container size, expressed in number of elements.
+			 * Member type size_type is an unsigned integral type.
+			 * 
+			 * @param val
+			 * Object whose content is copied to the added elements
+			 * in case that n is greater than the current container size.
+			 * If not specified, the default constructor is used instead.
+			 */
+			void resize(size_type n, value_type val = value_type())
+			{
+				if (n > this->max_size())
+					throw (std::out_of_range("ft::vector"));
+				else if (n < this->size())
+					while (this->size() > n)
+						this->_alloc.destroy(--this->_end);
+				else
+					this->insert(this->_end, n - this->size(), val);
+			};
+
+			/**
+			 * @brief Return size of allocated storage capacity
+			 * Returns the size of the storage space currently allocated for the vector,
+			 * expressed in terms of elements.
+			 */
+			size_type capacity() const {return (this->_end_capacity - this->_start); };
+
+			/**
+			 * @brief Test whether vector is empty
+			 * Returns whether the vector is empty (i.e. whether its size is 0).
+			 * This function does not modify the container in any way.
+			 * To clear the content of a vector, see vector::clear.
+			 * 
+			 * @return true if the container size is 0, false otherwise.
+			 */
+			bool empty() const {return (this->size == 0); };
+
+			/**
+			 * @brief Request a change in capacity
+			 * Requests that the vector capacity be at least enough to contain n elements.
+			 * 
+			 * @param n
+			 * Minimum capacity for the vector.
+			 * Note that the resulting vector capacity may be equal or greater than n.
+			 * Member type size_type is an unsigned integral type.
+			 */
+			void reserve(size_type n)
+			{
+				if (n > this->max_size())
+					throw (std::out_of_range("ft::vector"));
+				if (this->capacity() == n)
+					return ;
+				pointer prev_start = this->_start;
+				pointer prev_end = this->_end;
+				size_type prev_size = this->size();
+				size_type prev_capacity = this->capacity();
+
+				this->_start = this->_alloc.allocate(n);
+				this->_end_capacity = this->_start + n;
+				this->_end = this->_start;
+				
+				for (pointer idx = prev_start; idx != prev_end; idx++)
+					this->_alloc.construct(this->_end++, *idx);
+				
+				for (size_type len = (prev_end - prev_start); len > 0; len--)
+					this->_alloc.destroy(--prev_end);
+				
+				this->_alloc.deallocate(prev_start, prev_capacity);
+			};
+
+			/**
+			 * @brief Access element
+			 * Returns a reference to the element at position n in the vector container.
+			 * 
+			 * @param n
+			 * Position of an element in the container.
+			 * Notice that the first element has a position of 0 (not 1).
+			 */
+			reference operator[] (size_type n) {return (*(_start + n)); };
+
+			/**
+			 * @brief Access element
+			 * Returns a reference to the element at position n in the vector container.
+			 * 
+			 * @param n
+			 * Position of an element in the container.
+			 * Notice that the first element has a position of 0 (not 1).
+			 */
+			const_reference operator[] (size_type n) const {return (*(_start + n)); };
+
+			/**
+			 * @brief Access element
+			 * Returns a reference to the element at position n in the vector.
+			 * 
+			 * @param n Position of an element in the container.
+			 * If this is greater than, or equal to, the vector size,
+			 * an exception of type out_of_range is thrown.
+			 * Notice that the first element has a position of 0 (not 1).
+			 */
+			reference at(size_type n)
+			{
+				if (this->size() <= n)
+					throw (std::out_of_range("ft::vector"));
+				return (*(_start + n));
+			};
+
+			/**
+			 * @brief Access element
+			 * Returns a reference to the element at position n in the vector.
+			 * 
+			 * @param n Position of an element in the container.
+			 * If this is greater than, or equal to, the vector size,
+			 * an exception of type out_of_range is thrown.
+			 * Notice that the first element has a position of 0 (not 1).
+			 */
+			const_reference at(size_type n)
+			{
+				if (this->size() <= n)
+					throw (std::out_of_range("ft::vector"));
+				return (*(_start + n));
+			};
+
+			/**
+			 * @brief Access first element
+			 * Returns a reference to the first element in the vector.
+			 */
+			reference front() {return (*_start); };
+
+			/**
+			 * @brief Access first element
+			 * Returns a reference to the first element in the vector.
+			 */
+			const_reference front() const {return (*_start); };
+
+			/**
+			 * @brief Access last element
+			 * Returns a reference to the last element in the vector.
+			 */
+			reference back() {return (*(_end - 1)); };
+
+			/**
+			 * @brief Access last element
+			 * Returns a reference to the last element in the vector.
+			 */
+			const_reference back() const {return (*(_end - 1)); };
 	};
 }
 
