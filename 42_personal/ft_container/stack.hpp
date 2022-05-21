ff7f1ab5c37fc2537b6d6f3af52450eb429439d6
siverNa <dhrru123@naver.com>
2022-05-22 00:17:21 +0900

    Fix: 수정 stack.hpp 코드
    - protect 변수명 변경
    - 멤버 함수들 추가

--- 42_personal/ft_container/stack.hpp
+++ 42_personal/ft_container/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 00:06:56 by sna               #+#    #+#             */
-/*   Updated: 2022/05/21 23:38:21 by sna              ###   ########.fr       */
+/*   Updated: 2022/05/22 00:16:49 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ namespace ft {
 			typedef unsigned int		size_type;
 
 		protected:
-			container_type				c_t;
+			container_type				_c_t;
 
 		public:
 			/**
@@ -46,10 +46,72 @@ namespace ft {
 			 */
 			explicit stack (const container_type& ctnr = container_type())
 			{
-				c_t = ctnr;
+				_c_t = ctnr;
 			};
 
-			
+			/**
+			 * @brief Test whether container is empty
+			 * Returns whether the stack is empty: i.e. whether its size is zero.
+			 * This member function effectively calls member empty of the underlying container object.
+			 * 
+			 * @return true if the underlying container's size is 0, false otherwise.
+			 */
+			bool empty() const {return (_c_t.empty()); };
+
+			/**
+			 * @brief Return size
+			 * Returns the number of elements in the stack.
+			 * This member function effectively calls member size of the underlying container object.
+			 * 
+			 * @return The number of elements in the underlying container.
+			 */
+			size_type size() const {return (_c_t.size()); };
+
+			/**
+			 * @brief Access next element
+			 * Returns a reference to the top element in the stack.
+			 * Since stacks are last-in first-out containers,
+			 * the top element is the last element inserted into the stack.
+			 * This member function effectively calls member back of the underlying container object.
+			 * 
+			 * @return A reference to the top element in the stack.
+			 */
+			value_type& top() {return (_c_t.back()); };
+
+			/**
+			 * @brief Access next element
+			 * Returns a contst reference to the top element in the stack.
+			 * Since stacks are last-in first-out containers,
+			 * the top element is the last element inserted into the stack.
+			 * This member function effectively calls member back of the underlying container object.
+			 * 
+			 * @return A reference to the top element in the stack.
+			 */
+			const value_type& top() const {return (_c_t.back()); };
+
+			/**
+			 * @brief Insert element
+			 * Inserts a new element at the top of the stack,
+			 * above its current top element. The content of this new element is initialized to a copy of val.
+			 * This member function effectively calls the member function push_back
+			 * of the underlying container object.
+			 * 
+			 * @param val Value to which the inserted element is initialized.
+			 */
+			void push (const value_type& val) {_c_t.push_back(val); };
+
+			/**
+			 * @brief Remove top element
+			 * Removes the element on top of the stack, effectively reducing its size by one.
+			 * 
+			 * The element removed is the latest element inserted into the stack,
+			 * whose value can be retrieved by calling member stack::top.
+			 * 
+			 * This calls the removed element's destructor.
+			 * 
+			 * This member function effectively calls the member function pop_back of the underlying container object.
+			 */
+			void pop() {_c_t.pop_back(); };
 	};
 
 };//namespace ft
