9b962adf2011c4638b9e5a9878c57fd8f54d4975
siverNa <dhrru123@naver.com>
2022-05-22 22:39:46 +0900

    Feat: 추가 util.hpp 코드
    - pair 구조체 생성 및 생성자, 멤버 변수 및 함수, 관계 연산자 구현
    - make_pair 구현

--- 42_personal/ft_container/util.hpp
+++ 42_personal/ft_container/util.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 20:13:36 by sna               #+#    #+#             */
-/*   Updated: 2022/05/08 21:01:32 by sna              ###   ########.fr       */
+/*   Updated: 2022/05/22 22:37:59 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,144 @@ namespace ft
 		for (; first != last; first++)
 			++n;
 		return (n);
+	};
+
+	/**
+	 * @brief Pair of values
+	 * This class couples together a pair of values, 
+	 * which may be of different types (T1 and T2).
+	 * The individual values can be accessed through its public members first and second.
+	 * Pairs are a particular case of tuple.
+	 * 
+	 * @tparam T1 Type of member first, aliased as first_type.
+	 * @tparam T2 Type of member second, aliased as second_type.
+	 */
+	template <class T1, class T2>
+	struct pair {
+		public:
+			typedef T1			first_type;
+			typedef T2			second_type;
+
+			first_type			first;
+			second_type			second;
+
+			/**
+			 * @brief Construct pair (1) default constructor
+			 * Constructs a pair object.
+			 * This involves individually constructing its two component objects, 
+			 * with an initialization that depends on the constructor form invoked:
+			 * 
+			 * Constructs a pair object with its elements value-initialized.
+			 */
+			pair() : first(), second() {};
+
+			/**
+			 * @brief Construct pair (2) copy / move constructor (and implicit conversion)
+			 * Constructs a pair object.
+			 * This involves individually constructing its two component objects, 
+			 * with an initialization that depends on the constructor form invoked:
+			 * 
+			 * The object is initialized with the contents of the pr pair object.
+			 * The corresponding member of pr is passed to the constructor of each of its members.
+			 * 
+			 * @param pr Another pair object.
+			 */
+			template<class U, class V>
+			pair (const pair<U, V>& pr) : first(pr.first), second(pr.second) {};
+
+			/**
+			 * @brief Construct pair (3) initialization constructor
+			 * Constructs a pair object.
+			 * This involves individually constructing its two component objects, 
+			 * with an initialization that depends on the constructor form invoked:
+			 * 
+			 * Member first is constructed with a and member second with b.
+			 * 
+			 * @param a An object of the type of first, or some other type implicitly convertible to it.
+			 * @param b An object of the type of second, or some other type implicitly convertible to it.
+			 */
+			pair (const first_type& a, const second_type& b) : first(a), second(b) {};
+
+			/**
+			 * @brief Assign contents
+			 * Assigns pr as the new content for the pair object.
+			 * Member first is assigned pr.first, and member second is assigned pr.second.
+			 */
+			pair& operator= (const pair& pr)
+			{
+				if (*this == pr)
+					return (*this);
+				first = pr.first;
+				second = pr.second;
+				return (*this);	
+			};
+	};
+
+	/**
+	 * @brief Relational operators for pair
+	 * Performs the appropriate comparison operation between the pair objects lhs and rhs.
+	 * 
+	 * Two pair objects compare equal to each other if both their first members compare equal to each other 
+	 * and both their second members compare also equal to each other
+	 * (in both cases using operator== for the comparison).
+	 * 
+	 * Similarly, operators <, >, <= and >= perform a lexicographical comparison on the sequence formed
+	 * by members first and second (in all cases using operator< reflexively for the comparisons).
+	 */
+	template <class T1, class T2>
+	bool operator== (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	};
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return (!(lhs == rhs));
+	};
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && (lhs.second < rhs.second)));
+	};
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return (!(rhs < lhs));
+	};
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return (rhs < lhs);
+	};
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return (!(lhs < rhs));
+	};
+
+	/**
+	 * @brief Construct pair object
+	 * Constructs a pair object with its first element set to x and its second element set to y.
+	 * 
+	 * The template types can be implicitly deduced from the arguments passed to make_pair.
+	 * 
+	 * pair objects can be constructed from other pair objects containing different types,
+	 * if the respective types are implicitly convertible.
+	 * 
+	 * @param x Values for the members first, respectively, of the pair object being constructed.
+	 * @param y Values for the members second, respectively, of the pair object being constructed.
+	 * 
+	 * @return A pair object whose elements first and second are set to x and y respectivelly.
+	 */
+	template <class T1, class T2>
+	pair<T1, T2> make_pair (T1 x, T2 y)
+	{
+		return (pair<T1, T2>(x, y));
 	};
 }
 
