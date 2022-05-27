06112d42c0df25cf9288efaf84559b13a61eb75a
siverNa <dhrru123@naver.com>
2022-05-27 21:07:39 +0900

    Feat: 추가 tree_iterator 코드
    - tree iterator 맴버타입 변수 추가

--- 42_personal/ft_container/tree_iterator.hpp
+++ 42_personal/ft_container/tree_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 22:24:25 by sna               #+#    #+#             */
-/*   Updated: 2022/05/26 22:53:11 by sna              ###   ########.fr       */
+/*   Updated: 2022/05/27 20:57:11 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,15 @@ struct Node {
 		virtual ~Node() {};
 };
 
-
+template <typename T>
+class tree_iterator : public ft::iterator<ft::bidirectional_iterator_tag, T> {
+	public:
+		typedef typename ft::iterator<ft::bidirectional_iterator_tag, T>::iterator_category		iterator_category;
+		typedef typename ft::iterator_traits<T*>::value_type									value_type;
+		typedef typename ft::iterator_traits<T*>::difference_type								difference_type;
+		typedef typename ft::iterator_traits<T*>::pointer										pointer;
+		typedef typename ft::iterator_traits<T*>::reference										reference;
+		
+};
 
 #endif
