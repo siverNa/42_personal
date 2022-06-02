63d26b94fc42ffb97b23f967e3e0861f6ea0835c
siverNa <dhrru123@naver.com>
2022-06-02 23:10:55 +0900

    Feat: 추가 tree_iterator.hpp 코드
    - tree_iterator 클래스 생성
    - 멤버 변수 및 함수, 생성자 및 소멸자 등등 추가

--- 42_personal/ft_container/tree_iterator.hpp
+++ 42_personal/ft_container/tree_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 22:24:25 by sna               #+#    #+#             */
-/*   Updated: 2022/05/27 20:57:11 by sna              ###   ########.fr       */
+/*   Updated: 2022/06/02 22:01:02 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,126 @@ class tree_iterator : public ft::iterator<ft::bidirectional_iterator_tag, T> {
 		typedef typename ft::iterator_traits<T*>::difference_type								difference_type;
 		typedef typename ft::iterator_traits<T*>::pointer										pointer;
 		typedef typename ft::iterator_traits<T*>::reference										reference;
-		
+		typedef Node<typename ft::remove_const<value_type>::type >*								node_pointer;
+
+	private:
+		node_pointer	_node;
+
+		node_pointer tree_min(node_pointer n) const {
+			while (n->left != ft::u_nullptr && !n->left->is_nil)
+				n = n->left;
+			return (n);
+		};
+
+		node_pointer tree_max(node_pointer n) const {
+			while (!n->right->is_nil)
+				n = n->right;
+			return (n);
+		};
+	
+	public:
+		tree_iterator() {};
+
+		tree_iterator(void *node) : _node(static_cast<node_pointer>(node)) {};
+
+		tree_iterator(const tree_iterator<typename ft::remove_const<value_type>::type >& other) {*this = other; };
+
+		tree_iterator& operator=(const tree_iterator<typename ft::remove_const<value_type>::type >& other)
+		{
+			this->_node = other.get_node();
+			return (*this);
+		};
+
+		reference operator*() const {
+			return (*(_node->value));
+		};
+
+		pointer operator->() const {
+			return (_node->value);
+		};
+
+		/**
+		 * @brief 전위 증가 연산자
+		 */
+		tree_iterator& operator++() {
+			if (_node->right && !_node->right->is_nil)
+				_node = tree_min(_node->right);
+			else
+			{
+				node_pointer p = _node->parent;
+				while (p != ft::u_nullptr && _node == p->right)
+				{
+					_node = p;
+					p = p->parent;
+				}
+				_node = p;
+			}
+			return (*this);
+		};
+
+		tree_iterator operator++(int) {
+			tree_iterator<value_type> temp = *this;
+			if (!_node->right->is_nil)
+				_node = tree_min(_node->right);
+			else
+			{
+				node_pointer p = _node->parent;
+				while (p != ft::u_nullptr && _node = p->right)
+				{
+					_node = p;
+					p = p->parent;
+				}
+				_node = p;
+			}
+			return (temp);
+		};
+
+		tree_iterator& operator--() {
+			if (_node->left && !_node->left->is_nil)
+				_node = tree_max(_node->left);
+			else
+			{
+				node_pointer p = _node->parent;
+				while (p != ft::u_nullptr && _node == p->left)
+				{
+					_node = p;
+					p = p->parent;
+				}
+				_node = p;
+			}
+			return (*this);
+		};
+
+		tree_iterator oparator--(int) {
+			tree_iterator<value_type> temp = *this;
+			if (_node->left && !_node->left->is_nil)
+				_node = tree_max(_node->left);
+			else
+			{
+				node_pointer p = _node->parent;
+				while (p != ft::u_nullptr && _node == p->left)
+				{
+					_node = p;
+					p = p->parent;
+				}
+				_node = p;
+			}
+			return (temp);
+		};
+
+		node_pointer get_node() const {return (_node); };
 };
+
+template <typename T1, typename T2>
+bool operator==(const tree_iterator<T1>& lhs, const tree_iterator<T2>& rhs)
+{
+	return (lhs.get_node() == rhs.get_node());
+}
+
+template <typename T1, typename T2>
+bool operator!=(const tree_iterator<T1>& lhs, const tree_iterator<T2>& rhs)
+{
+	return (lhs.get_node() != rhs.get_node());
+}
 
 #endif
