0ab26c8e817e3842edcb088eb7e5ff4163106a48
siverNa <dhrru123@naver.com>
2022-06-09 20:27:35 +0900

    Feat: 수정 map.hpp 코드
    - lower, upper, equal_bound 함수 구현
    - get_allocator 및 다른 함수들 구현

--- 42_personal/ft_container/map.hpp
+++ 42_personal/ft_container/map.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 00:37:01 by sna               #+#    #+#             */
-/*   Updated: 2022/06/08 21:59:34 by sna              ###   ########.fr       */
+/*   Updated: 2022/06/09 20:18:17 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,43 +97,6 @@ namespace ft {
 			map (const map& x)
 				: _alloc(x._alloc), _tree(x._tree), _compare(x._compare)
 			{};
-
-			/**
-			 * @brief Insert elements
-			 * Extends the container by inserting new elements,
-			 * effectively increasing the container size by the number of elements inserted.
-			 * 
-			 * @param val Value to be copied to (or moved as) the inserted element.
-			 */
-			ft::pair<iterator, bool> insert(const value_type& val) {
-				return (_tree.insert(val));
-			};
-
-			/**
-			 * @brief Insert elements
-			 * Extends the container by inserting new elements,
-			 * effectively increasing the container size by the number of elements inserted.
-			 * 
-			 * @param position Hint for the position where the element can be inserted.
-			 * @param val Value to be copied to (or moved as) the inserted element.
-			 */
-			iterator insert (iterator position, const value_type& val) {
-				return (_tree.insert(position, val));
-			};
-
-			/**
-			 * @brief Insert elements
-			 * Extends the container by inserting new elements,
-			 * effectively increasing the container size by the number of elements inserted.
-			 * 
-			 * @param first, last
-			 * Iterators specifying a range of elements.
-			 * Copies of the elements in the range [first,last) are inserted in the container.
-			 */
-			template <class InputIterator>
-			void insert (typename enable_if<!is_integral<InputIt>::value, InputIterator >::type first, InputIterator last) {
-				_tree.insert(first, last);
-			};
 			
 			/**
 			 * @brief Map destructor
@@ -290,6 +253,233 @@ namespace ft {
 			 */
 			size_type max_size() const {
 				return (_tree.max_size());
+			};
+
+			/**
+			 * @brief Access element
+			 * If k matches the key of an element in the container,
+			 * the function returns a reference to its mapped value.
+			 */
+			mapped_type& operator[] (const key_type& k) {
+				return (*((this->insert(ft::make_pair(k, mapped_type()))).first)).second;
+			};
+
+			/**
+			 * @brief Insert elements
+			 * Extends the container by inserting new elements,
+			 * effectively increasing the container size by the number of elements inserted.
+			 * 
+			 * @param val Value to be copied to (or moved as) the inserted element.
+			 */
+			ft::pair<iterator, bool> insert(const value_type& val) {
+				return (_tree.insert(val));
+			};
+
+			/**
+			 * @brief Insert elements
+			 * Extends the container by inserting new elements,
+			 * effectively increasing the container size by the number of elements inserted.
+			 * 
+			 * @param position Hint for the position where the element can be inserted.
+			 * @param val Value to be copied to (or moved as) the inserted element.
+			 */
+			iterator insert (iterator position, const value_type& val) {
+				return (_tree.insert(position, val));
+			};
+
+			/**
+			 * @brief Insert elements
+			 * Extends the container by inserting new elements,
+			 * effectively increasing the container size by the number of elements inserted.
+			 * 
+			 * @param first, last
+			 * Iterators specifying a range of elements.
+			 * Copies of the elements in the range [first,last) are inserted in the container.
+			 */
+			template <class InputIterator>
+			void insert (typename enable_if<!is_integral<InputIt>::value, InputIterator >::type first, InputIterator last) {
+				_tree.insert(first, last);
+			};
+
+			/**
+			 * @brief Erase elements
+			 * Removes from the map container either a single element or a range of elements ([first,last)).
+			 * This effectively reduces the container size by the number of elements removed, which are destroyed.
+			 * 
+			 * @param position Iterator pointing to a single element to be removed from the map.
+			 */
+			void erase (iterator position) {
+				_tree.erase(position);
+			};
+			
+			/**
+			 * @brief Erase elements
+			 * Removes from the map container either a single element or a range of elements ([first,last)).
+			 * This effectively reduces the container size by the number of elements removed, which are destroyed.
+			 * 
+			 * @param k Key of the element to be removed from the map.
+			 */
+			size_type erase (const key_type& k) {
+				return (_tree.erase(make_pair(k, mapped_type())));
+			};
+			
+			/**
+			 * @brief Erase elements
+			 * Removes from the map container either a single element or a range of elements ([first,last)).
+			 * This effectively reduces the container size by the number of elements removed, which are destroyed.
+			 * 
+			 * @param first, last
+			 * Iterators specifying a range within the map container to be removed: [first,last).
+			 */
+			void erase (iterator first, iterator last) {
+				_tree.erase(first, last);
+			};
+
+			/**
+			 * @brief Swap content
+			 * Exchanges the content of the container by the content of x,
+			 * which is another map of the same type. Sizes may differ.
+			 */
+			void swap (map& x) {
+				key_compare temp_compare = this->_compare;
+
+				this->_compare = x._compare;
+				x._compare = temp_compare;
+				_tree.swap(x._tree);
+			};
+
+			/**
+			 * @brief Clear content
+			 * Removes all elements from the map container (which are destroyed),
+			 * leaving the container with a size of 0.
+			 */
+			void clear() {
+				_tree.clear();
+			};
+
+			/**
+			 * @brief Return key comparison object
+			 * Returns a copy of the comparison object used by the container to compare keys.
+			 */
+			key_compare key_comp() const {
+				return (_compare);
+			};
+
+			/**
+			 * @brief Return value comparison object
+			 * Returns a comparison object that can be used to compare two elements
+			 * to get whether the key of the first one goes before the second.
+			 */
+			value_compare value_comp() const {
+				return (_tree.value_comp());
+			};
+
+			/**
+			 * @brief Get iterator to element
+			 * Searches the container for an element with a key equivalent to k
+			 * and returns an iterator to it if found, otherwise it returns an iterator to map::end.
+			 */
+			iterator find (const key_type& k) {
+				return (_tree.find(make_pair(k, mapped_type())));
+			};
+			
+			/**
+			 * @brief Get iterator to element
+			 * Searches the container for an element with a key equivalent to k
+			 * and returns an iterator to it if found, otherwise it returns an iterator to map::end.
+			 */
+			const_iterator find (const key_type& k) const {
+				return (_tree.find(make_pair(k, mapped_type())));
+			};
+
+			/**
+			 * @brief Count elements with a specific key
+			 * Searches the container for elements with a key equivalent to k
+			 * and returns the number of matches.
+			 * 
+			 * Because all elements in a map container are unique, the function can only return 1
+			 * (if the element is found) or zero (otherwise).
+			 * 
+			 * @param k Key to search for.
+			 */
+			size_type count (const key_type& k) const {
+				return (_tree.count(make_pair(k, mapped_type())));
+			};
+
+			/**
+			 * @brief Return iterator to lower bound
+			 * Returns an iterator pointing to the first element in the container
+			 * whose keyis not considered to go before k
+			 * (i.e., either it is equivalent or goes after).
+			 * 
+			 * @param k Key to search for.
+			 */
+			iterator lower_bound (const key_type& k) {
+				return (_tree.lower_bound(make_pair(k, mapped_type())));
+			};
+			
+			/**
+			 * @brief Return iterator to lower bound
+			 * Returns an iterator pointing to the first element in the container
+			 * whose keyis not considered to go before k
+			 * (i.e., either it is equivalent or goes after).
+			 * 
+			 * @param k Key to search for.
+			 */
+			const_iterator lower_bound (const key_type& k) const {
+				return (_tree.lower_bound(make_pair(k, mapped_type())));
+			};
+
+			/**
+			 * @brief Return iterator to upper bound
+			 * Returns an iterator pointing to the first element in the container
+			 * whose key is considered to go after k.
+			 * 
+			 * @param k Key to search for.
+			 */
+			iterator upper_bound (const key_type& k) {
+				return (_tree.upper_bound(make_pair(k, mapped_type())));
+			};
+			
+			/**
+			 * @brief Return iterator to upper bound
+			 * Returns an iterator pointing to the first element in the container
+			 * whose key is considered to go after k.
+			 * 
+			 * @param k Key to search for.
+			 */
+			const_iterator upper_bound (const key_type& k) const {
+				return (_tree.upper_bound(make_pair(k, mapped_type())));
+			};
+
+			/**
+			 * @brief Get range of equal elements
+			 * Returns the bounds of a range that includes all the elements in the container
+			 * which have a key equivalent to k.
+			 * 
+			 * @param k Key to search for.
+			 */
+			ft::pair<const_iterator,const_iterator> equal_range (const key_type& k) const {
+				return (_tree.equal_range(make_pair(k, mapped_type())));
+			};
+			
+			/**
+			 * @brief Get range of equal elements
+			 * Returns the bounds of a range that includes all the elements in the container
+			 * which have a key equivalent to k.
+			 * 
+			 * @param k Key to search for.
+			 */
+			ft::pair<iterator,iterator>				equal_range (const key_type& k) {
+				return (_tree.equal_range(make_pair(k, mapped_type())));
+			};
+
+			/**
+			 * @brief Get allocator
+			 * Returns a copy of the allocator object associated with the map.
+			 */
+			allocator_type get_allocator() const {
+				return (_tree.get_allocator());
 			};
 	};
 }//namespace ft
