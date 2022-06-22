f3acd87a43b4eba24ebebc9df45fbc6d25aa4190
siverNa <dhrru123@naver.com>
2022-06-22 14:02:50 +0900

    Feat: 추가 RBTree_node.hpp 코드
    - 생성자, 소멸자 등 추가
    - 노드에서 사용할 함수 추가
    - 관계 연산자 추가

--- 42_personal/ft_container/RBTree_node.hpp
+++ 42_personal/ft_container/RBTree_node.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 21:20:10 by sna               #+#    #+#             */
-/*   Updated: 2022/06/21 23:30:50 by sna              ###   ########.fr       */
+/*   Updated: 2022/06/22 13:59:23 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,100 @@ namespace ft {
 			rb_color	color;
 			Alloc		alloc;
 
-			
+			rb_node()
+				: value(NULL), left(NULL), right(NULL), parent(NULL), color(BLACK), alloc(Alloc())
+			{};
+
+			rb_node(const T& val)
+				: value(NULL), left(NULL), right(NULL), parent(NULL), color(RED), alloc(Alloc())
+			{
+				this->value = alloc.allocate(1);
+				alloc.construct(this->value, val);
+			};
+
+			rb_node(const rb_node& obj)
+				: value(NULL), left(NULL), right(NULL), parent(NULL), color(RED), alloc(Alloc())
+			{
+				if (!(obj.value == NULL))
+				{
+					this->value = alloc.allocate(1);
+					alloc.construct(this->value, obj.get_value());
+				}
+			};
+
+			~rb_node()
+			{
+				if (this->value != NULL)
+				{
+					alloc.destroy(this->value);
+					alloc.deallocate(this->value, 1);
+				}
+			};
+
+			T& get_value() const
+			{
+				return (*this->value);
+			};
+
+			bool empty() const
+			{
+				if (this->value == NULL)
+					return (true);
+				return (false);
+			};
+
+			bool is_root() const
+			{
+				if (this->parent->empty())
+					return (true);
+				return (false);
+			};
+
+			bool is_left() const
+			{
+				if (this->parent->left == this)
+					return (true);
+				else
+					return (false);
+			};
+
+			bool is_right() const
+			{
+				if (this->parent->right == this)
+					return (true);
+				else
+					return (false);
+			};
+
+			bool is_leaf() const
+			{
+				if (this->left->empty() && this->right->empty())
+					return (true);
+				return (false);
+			};
+
+			rb_node* sibling()//형제 노드
+			{
+				if (this == this->parent->left)//현재 자신이 왼쪽이라면
+					return (this->parent->right);//형제 노드는 오른쪽
+				else//자신이 오른쪽이면
+					return (this->parent->left);//형제 노드는 왼쪽
+			};
+
+			bool operator<(const rb_node& obj) const
+			{
+				return (*(this->value) < *obj.value);
+			};
+
+			bool operator>(const rb_node& obj) const
+			{
+				return (*this > obj);
+			};
+
+			bool operator==(const rb_node& obj) const
+			{
+				return (*(this->value) == *obj.value);
+			};
 	};
 }//namespace ft
 
