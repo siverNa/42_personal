8f3330bdc4bb081746ccb939f522377578a637da
siverNa <dhrru123@naver.com>
2022-06-23 21:33:30 +0900

    Feat: 추가 type_trait.hpp 코드
    - is_iter 및 is_pointer 추가

--- 42_personal/ft_container/type_trait.hpp
+++ 42_personal/ft_container/type_trait.hpp
@@ -6,7 +6,7 @@
 /*   By: sna <sna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 17:57:08 by sna               #+#    #+#             */
-/*   Updated: 2022/06/09 20:43:25 by sna              ###   ########.fr       */
+/*   Updated: 2022/06/23 21:17:36 by sna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,42 @@ namespace ft {
 	struct is_integral<unsigned long int> {static const bool value = true; };
 	template <>
 	struct is_integral<unsigned long long int> {static const bool value = true; };
+
+	template <typename T, typename U>
+	struct is_equal {static const bool value = false; };
+
+	template <typename T, typename Enable = void>
+	struct is_iter {static const bool value = false; };
+
+	template <typename T>
+	struct remove_volatile {typedef T type; };
+
+	template <typename T>
+	struct remove_volatile<volatile T>{typedef T type; };
+
+	template <typename T>
+	struct remove_cv : remove_const<typename remove_volatile<T>::type> {};
+
+	template <typename T>
+	struct is_unqualified_pointer{enum { value = false }; };
+
+	template <typename T>
+	struct is_unqualified_pointer<T*> {enum { value = true }; };
+
+	template <typename T>
+	struct is_pointer: is_unqualified_pointer<typename remove_cv<T>::type> {};
+
+	template <typename T>
+	struct is_iter<T, typename ft::enable_if<ft::is_equal<typename T::value_type, typename ft::iterator_traits<T>::value_type>::value>::type>
+	{
+		static const bool value = true;
+	};
+
+	template <typename T>
+	struct is_iter<T, typename ft::enable_if<ft::is_pointer<T>::value>::type>
+	{
+		static const bool value = true;
+	};
 
 }//namespace ft
 
